b60006d42b08652a53f48fd6226764d49e38fd10
Miriam Wiesner <miriamxyra@users.noreply.github.com>
2021-11-14 17:20:53 +0100

    Added Test_Add-Type.ps1

---
+++ Chapter05/Test_Add-Type.ps1
@@ -0,0 +1,28 @@
+﻿$Source = @"
+using System;
+using System.IO;
+
+public class DirectoryTest
+{
+
+    public static string[] GetDirectories(string path)
+    {
+        string[] dirs;
+
+        try
+        {
+            dirs = Directory.GetDirectories(@path, "*", SearchOption.TopDirectoryOnly);
+        }
+        catch (System.UnauthorizedAccessException)
+        {
+            dirs = new string[0];
+        }
+
+        return dirs;
+    }
+}
+"@
+
+Add-Type -TypeDefinition $Source
+
+[DirectoryTest]::GetDirectories("C:\")
