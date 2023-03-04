68fb6f76a16cfab55a817f633843888ebdb8bec1
Miriam Wiesner <miriamxyra@users.noreply.github.com>
2023-03-04 20:21:46 +0100

    Removed TestModule

--- Chapter02/TestModule/TestModule/changelog.md
+++
@@ -1,5 +0,0 @@
-﻿# Changelog
-## 1.0.0 (2020-11-15)
- - New: Some Stuff
- - Upd: Moar Stuff
- - Fix: Much Stuff
--- Chapter02/TestModule/azFunctionResources/clientModule/internal/configurations/connection.ps1
+++
@@ -1,3 +0,0 @@
-﻿Set-PSFConfig -Module 'TestModule' -Name 'Client.Uri' -Value $null -Initialize -Validation 'string' -Description "Url to connect to the TestModule Azure function"
-Set-PSFConfig -Module 'TestModule' -Name 'Client.UnprotectedToken' -Value '' -Initialize -Validation 'string' -Description "The unencrypted access token to the TestModule Azure function. ONLY use this from secure locations or non-sensitive functions!"
-Set-PSFConfig -Module 'TestModule' -Name 'Client.ProtectedToken' -Value $null -Initialize -Validation 'credential' -Description "An encrypted access token to the TestModule Azure function. Use this to persist an access token in a way only the current user on the current system can access."
