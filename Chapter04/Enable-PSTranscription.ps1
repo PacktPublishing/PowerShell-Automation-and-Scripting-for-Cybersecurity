1cc63507df2e19cfee72c7e4c62422b9ed56ee7f
Miriam Wiesner <miriamxyra@users.noreply.github.com>
2023-04-07 12:32:01 +0200

    Changed Enable-PSTranscription.ps1 to be script-only (removed function and example)

--- Chapter04/Enable-PSTranscription.ps1
+++ Chapter04/Enable-PSTranscription.ps1
@@ -1,35 +1,29 @@
-function Enable-PSTranscription {
-    <#
-        .SYNOPSIS
-        This function enables PowerShell transcription.
-    
-        .DESCRIPTION
-        This function enables PowerShell transcription on the local machine. This function configures EnableTranscription, EnableInvocationHeader and OutputDirectory and overwrites existing registry keys.
-    
-        .PARAMETER OutputDirectory
-        Using the mandatory parameter -OutputDirectory, you can specify where the transcript files should be saved.
-    
-        .EXAMPLE
-        Enable-PSTranscription -OutputDirectory "C:\tmp"
-    
-        Enables PowerShellTranscription and writes all output to C:\tmp.
-    #>
-    
-    [cmdletbinding()]
-    param (
-        [Parameter(Mandatory)]
-        [string]$OutputDirectory
-    )
+<#
+.SYNOPSIS
+This script enables PowerShell transcription.
 
-    $registryPath = "HKLM:\SOFTWARE\WOW6432Node\Policies\Microsoft\Windows\PowerShell\Transcription"
+.DESCRIPTION
+This script enables PowerShell transcription on the local machine. It configures the registry keys for EnableTranscription, EnableInvocationHeader, and OutputDirectory. You can specify the output directory using the -OutputDirectory parameter. By default, the output directory is set to c:\ProgramData\WindowsPowerShell\Transcripts.
 
-    if (!(Test-Path $registryPath)) {
-        New-Item $registryPath -Force
-    }
+.PARAMETER OutputDirectory
+Specifies the directory where the transcript files should be saved.
 
-    New-ItemProperty $registryPath -Name "EnableTranscripting" -PropertyType Dword -Value 1 -Force
-    New-ItemProperty $registryPath -Name "EnableInvocationHeader" -PropertyType Dword -Value 1 -Force
-    New-ItemProperty $registryPath -Name "OutputDirectory" -PropertyType String -Value $OutputDirectory -Force
+.EXAMPLE
+Enable-PSTranscription.ps1 -OutputDirectory "C:\PSLogs"
+#>
+
+[cmdletbinding()]
+param (
+    [Parameter()]
+    [string]$OutputDirectory = "c:\ProgramData\WindowsPowerShell\Transcripts"
+)
+
+$registryPath = "HKLM:\SOFTWARE\WOW6432Node\Policies\Microsoft\Windows\PowerShell\Transcription"
+
+if (!(Test-Path $registryPath)) {
+    New-Item $registryPath -Force
 }
 
-Enable-PSTranscription -OutputDirectory "C:\tmp"
+New-ItemProperty $registryPath -Name "EnableTranscripting" -PropertyType Dword -Value 1 -Force
+New-ItemProperty $registryPath -Name "EnableInvocationHeader" -PropertyType Dword -Value 1 -Force
+New-ItemProperty $registryPath -Name "OutputDirectory" -PropertyType String -Value $OutputDirectory -Force
