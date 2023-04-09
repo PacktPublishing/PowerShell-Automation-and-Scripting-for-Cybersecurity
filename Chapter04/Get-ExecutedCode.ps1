d3b2a719f84baae10b696be7eea26c76e47ab564
Miriam Wiesner <miriamxyra@users.noreply.github.com>
2023-04-09 14:06:29 +0200

    Improved Get-ExecutedCode to automatically reassemble scripts that were split into multiple ScriptBlocks (IsMultiPart -eq True)

--- Chapter04/Get-ExecutedCode.ps1
+++ Chapter04/Get-ExecutedCode.ps1
@@ -1,66 +1,55 @@
 function Get-ExecutedCode {
     <#
-        .SYNOPSIS
-        This function queries the EventLog and outputs all code that was executed on this system.
-            
-        .DESCRIPTION
-        This function queries the EventLog and outputs all code that was executed on this system.
-
-        .PARAMETER SearchWord
-        Outputs all occurrences where the SearchWord was found in the executed code.
-
-        .PARAMETER UserId
-        Outputs all events that were caused by the user with the specified user id.
-
-        .PARAMETER Level
-        Outputs all events containing the specified level.
-
-        .PARAMETER Path
-        Outputs all code that was executed in the specified path or a subfolder.
-
-        .PARAMETER IsMultiPart
-        Can be used to specify if all code that was split into multiple event ids (IsMultiPart = $true) should be displayed or if only non-multipart events should be displayed.
-
+    .SYNOPSIS
+        Queries the EventLog and outputs all code that was executed on the system.
+        
+    .DESCRIPTION
+        This function queries the EventLog and outputs all code that was executed on this system. It can be used to find executed code with a specific keyword, user ID, log level, or path. 
     
-        .EXAMPLE
+    .PARAMETER SearchWord
+        Filters events containing the specified keyword in the executed code.
+    
+    .PARAMETER UserId
+        Filters events caused by the user with the specified user ID.
+    
+    .PARAMETER Level
+        Filters events containing the specified log level.
+    
+    .PARAMETER Path
+        Filters events where the executed code was in the specified path or subfolder.
+        
+    .EXAMPLE
         Get-ExecutedCode -SearchWord "Set-"
+        Outputs all code that was executed on the system which contains the keyword "Set-" in the code part.
     
-        Outputs all code that was executed on this system which contains the keyword "Set-" in the code part.
-
-
+    .EXAMPLE
         Get-ExecutedCode -UserId "S-1-5-18"
-
-        Finds all code that was executed by the user with the user id "S-1-5-18"
-
-
-        Get-ExecutedCode -Level "Warning"
-
-        Finds all events with the event level "Warning".
-
-
-        Get-ExecutedCode -Path "C:\Users\Administrator\Documents\GitHub\PowerShell-Automation-and-Scripting-for-CyberSecurity\Chapter04*"
-
-        Finds all code that was executed in a certain path.
-
-
-        Get-ExecutedCode -IsMultiPart $true
-
-        Finds all events, for which the code was split into multiple events.
-    #>
+        Finds all code that was executed by the user with the user ID "S-1-5-18".
     
+    .EXAMPLE
+        Get-ExecutedCode -Level "Warning"
+        Finds all events with the log level "Warning".
+    
+    .EXAMPLE
+        Get-ExecutedCode -Path "C:\Users\Administrator\Documents\GitHub\PowerShell-Automation-and-Scripting-for-CyberSecurity\Chapter04*"
+        Finds all code that was executed in a certain path.
+    
+    #>
+        
     [cmdletbinding()]
     param (
         [string]$SearchWord,
         [string]$UserId,
         [string]$Level,
-        [string]$Path,
-        [Boolean]$IsMultiPart
+        [string]$Path
     )
-
+    
     $PSWinEventLog = @{ ProviderName = "Microsoft-Windows-PowerShell"; Id = 4104 }
     $PSCoreEventLog = @{ ProviderName = "PowerShellCore"; Id = 4104 }
-
-    $CodeEvents = Get-WinEvent -FilterHashtable $PSWinEventLog, $PSCoreEventLog | ForEach-Object {
+    
+    $AllEvents = Get-WinEvent -FilterHashtable $PSWinEventLog, $PSCoreEventLog |
+    Sort-Object @{Expression = { $_.Properties[3].Value } }, @{Expression = { $_.Properties[0].Value } } |
+    ForEach-Object {
         [PSCustomObject]@{
             TimeCreated   = $_.TimeCreated
             ExecutedCode  = $_.Properties[2].Value
@@ -69,51 +58,76 @@ function Get-ExecutedCode {
             Path          = $_.Properties[4].Value
             ProviderName  = $_.ProviderName
             ScriptblockId = $_.Properties[3].Value
-            
-            #Is code split into multiple log entries?
             IsMultiPart   = $_.Properties[1].Value -ne 1
             CurrentPart   = $_.Properties[0].Value
             TotalParts    = $_.Properties[1].Value
         }
     }
-
+    
+    $oldScriptBlockId = ""
+    
+    # All Multipart Events
+    $CodeEvents = $AllEvents | Where-Object { $_.IsMultiPart } | foreach {
+        # Only merge the code if it's a new ScriptBlock
+        if ($oldScriptBlockId -ne $_.ScriptblockId) {
+            $ScriptBlockCode = $_.ExecutedCode
+            $oldScriptBlockId = $_.ScriptblockId
+        }
+        else {
+            $ScriptBlockCode += $_.ExecutedCode
+        }
+        # If it's the last part of the ScriptBlock, merge the code
+        if ($_.CurrentPart -eq $_.TotalParts) {
+            [PSCustomObject]@{
+                TimeCreated   = $_.TimeCreated
+                ExecutedCode  = $ScriptBlockCode
+                UserId        = $_.UserId
+                Level         = $_.Level
+                Path          = $_.Path
+                ProviderName  = $_.ProviderName
+                ScriptblockId = $_.ScriptblockId
+            }
+        }
+    }
+    
+    # All Regular Events
+    $CodeEvents += $AllEvents | Where-Object { !( $_.IsMultiPart) } | foreach {
+        # just add every event
+        [PSCustomObject]@{
+            TimeCreated   = $_.TimeCreated
+            ExecutedCode  = $ScriptBlockCode
+            UserId        = $_.UserId
+            Level         = $_.Level
+            Path          = $_.Path
+            ProviderName  = $_.ProviderName
+            ScriptblockId = $_.ScriptblockId
+        }
+    }
+    
+    $CodeEvents = $CodeEvents | Sort-Object TimeCreated 
+    
     #Filter for Search Word
     if ($SearchWord) {
         $CodeEvents = $CodeEvents | Where-Object { $_.ExecutedCode -match $SearchWord }
     }
-
+    
     #Filter for User Id
     if ($UserId) {
         $CodeEvents = $CodeEvents | Where-Object { $_.UserId -eq $UserId }
     }
-
+    
     #Filter for Level
     if ($Level) {
         $CodeEvents = $CodeEvents | Where-Object { $_.Level -eq $Level }
     }
-
+    
     #Filter for Path
     if ($Path) {
         $CodeEvents = $CodeEvents | Where-Object { $_.Path -like "$Path*" }
     }
-
-    #Filter if code was split into multiple event entries
-    if ($IsMultiPart) {
-        $CodeEvents = $CodeEvents | Where-Object { $_.IsMultiPart -eq $true }
-    }
-    else {
-        $CodeEvents = $CodeEvents | Where-Object { $_.IsMultiPart -eq $false }
-    }
-
+    
     return $CodeEvents
-
+    
 }
-
+    
 Get-ExecutedCode
-
-#Examples
-Get-ExecutedCode -SearchWord "Set-"
-Get-ExecutedCode -UserId "S-1-5-18"
-Get-ExecutedCode -Level "Warning"
-Get-ExecutedCode -Path "C:\Users\Administrator\Documents\GitHub\PowerShell-Automation-and-Scripting-for-CyberSecurity\Chapter04"
-Get-ExecutedCode -IsMultiPart $false
