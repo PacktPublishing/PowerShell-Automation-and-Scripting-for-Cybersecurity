4e8559f41d5f9c4cb1dc2ae51cc98bc33805d6c1
Miriam Wiesner <miriamxyra@users.noreply.github.com>
2023-05-20 16:25:16 +0200

    Added RunPoSh

---
+++ Chapter05/RunPoSh/RunPoSh/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Management.Automation;
+using System.Collections.ObjectModel;
+
+namespace RunPoSh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (PowerShell PoSh = PowerShell.Create())
+            {
+                if (args.Length >= 1)
+                {
+                    String totalArgs = "";
+                    for (int argCnt = 0; argCnt < args.Length; argCnt++)
+                    {
+                        if (totalArgs=="")
+                        {
+                            totalArgs += args[argCnt];
+                        }
+                        else
+                        {
+                            totalArgs += " " + args[argCnt];
+                        }
+                    }
+                    PoSh.AddScript(totalArgs);
+                    Collection<PSObject> PSOutput;
+                    PSOutput = PoSh.Invoke();
+                    foreach (PSObject outputItem in PSOutput)
+                    {
+                        if (outputItem != null)
+                        {
+                            Console.WriteLine(outputItem);
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No input was specified. Please specify a command to execute.\r\nExample: RunPoSh.exe Get-Process");
+                }
+            }
+        }
+    }
+}
